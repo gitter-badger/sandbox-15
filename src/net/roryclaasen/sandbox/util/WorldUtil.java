c942cc2f9559f2feff37ef8f4cd2d172921fc97a
GOGO98901 <gogo98901@live.co.uk>
2016-04-11 19:19:05 +0100

    Copyright and font stuff

--- src/net/roryclaasen/sandbox/util/WorldUtil.java
+++ src/net/roryclaasen/sandbox/util/WorldUtil.java
@@ -1,5 +1,22 @@
+/** 
+   Copyright 2016 Rory Claasen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
 package net.roryclaasen.sandbox.util;
 
+import net.gogo98901.log.Log;
 import net.roryclaasen.sandbox.DisplayManager;
 import net.roryclaasen.sandbox.Sandbox;
 import net.roryclaasen.sandbox.RenderEngine.skybox.Skybox;
@@ -28,6 +45,7 @@ public class WorldUtil {
 		if (Mouse.isButtonDown(0) && game.entityManager.getPlayer().isInMenu()) game.entityManager.getPlayer().setInMenu(false);
 
 		if (Sandbox.getArguments().isDebugMode()) {
+			Log.info("Lol");
 			if (Keyboard.isKeyDown(Keyboard.KEY_F1)) setWireFrame(false);
 			if (Keyboard.isKeyDown(Keyboard.KEY_F2)) setWireFrame(true);
 			if (Keyboard.isKeyDown(Keyboard.KEY_F9)) Skybox.setDay();
