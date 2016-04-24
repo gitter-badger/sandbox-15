d3fd94e54f1353930d8d0428aeece1a94bf35f42
GOGO98901 <gogo98901@live.co.uk>
2016-04-24 12:42:57 +0100

    removed old files

--- assets/script/main.js
+++ assets/script/main.js
@@ -40,4 +40,15 @@ $(window).load(function() {
 			$(this).hide();
 		});
 	});
+	Prism.plugins.NormalizeWhitespace.setDefaults({
+		'remove-trailing': true,
+		'remove-indent': true,
+		'left-trim': true,
+		'right-trim': true,
+		/*'break-lines': 80,
+		'indent': 2,
+		'remove-initial-line-feed': false,
+		'tabs-to-spaces': 4,
+		'spaces-to-tabs': 4*/
+	});
 });
