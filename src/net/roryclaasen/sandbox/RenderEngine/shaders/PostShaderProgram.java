5dd921f7be197d1ccdbbb1c44736291ea5ad3e31
GOGO98901 <gogo98901@live.co.uk>
2016-04-24 11:53:51 +0100

    added pages

--- src/net/roryclaasen/sandbox/RenderEngine/shaders/PostShaderProgram.java
+++
@@ -1,9 +0,0 @@
-package net.roryclaasen.sandbox.RenderEngine.shaders;
-
-
-public abstract class PostShaderProgram extends ShaderProgram {
-
-	public PostShaderProgram(String vert, String frag) {
-		super("post/" + vert, "post/" + frag);
-	}
-}
